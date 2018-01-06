56430eac9ac663579d80c8cc697cad8a3f4ec412
jdgrillone <jdgrillone@gmail.com>
2018-01-06 12:39:57 -0700

    added front end css and js files

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,57 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function () {
+  $(".change-status").on("click", function (event) {
+    var id = $(this).data("id");
+    var newStatus = $(this).data("status");
+
+    var newState = {
+      devoured: newStatus
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newState
+    }).then(
+      function () {
+        console.log("changed status to", newState);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+      );
+  });
+
+  $(".create-form").on("submit", function (event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    var newBurger = {
+      name: $("#ca").val().trim(),
+    };
+
+    // Send the POST request.
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function () {
+        console.log("created new burger");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+      );
+  });
+
+  $(".delete").on("click", function (event) {
+    var id = $(this).data("id");
+    console.log(id);
+    $.ajax("/api/burgers" + id, {
+      type: "DELETE"
+    }).then(
+    function () {
+      console.log("Deleted ID", id);
+      location.reload();
+    }
+    );
+});
+});
