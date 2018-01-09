826fac0d222916c67d6bd7633b5bf0ead31c6629
jdgrillone <jdgrillone@gmail.com>
2018-01-08 17:46:31 -0700

    Altered db schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,13 @@
-CREATE DATABASE burgers_db;
-USE burgers_db;
+DROP DATABASE IF EXISTS lr7ji4dahnkxuinu;
+
+CREATE DATABASE lr7ji4dahnkxuinu;
+USE lr7ji4dahnkxuinu;
 
 CREATE TABLE burgers
 (
     id int NOT NULL AUTO_INCREMENT,
     burger_name varchar(255) NOT NULL,
     devoured BOOLEAN DEFAULT false,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id)
 );
